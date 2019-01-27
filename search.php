a4456f754456e00b96d1efff18a93931edccbb28
Faruq <lekside1@gmail.com>
2019-01-26 23:59:02 -0500

    chnage to search

--- search.php
+++ search.php
@@ -9,29 +9,33 @@ $username = "root";
 $user_pass = "password";
 $database = "dictionary";
 
-$mysqli = new mysqli($host, "", "", $database);
+$mysqli = new mysqli($host, $username, "", $database);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 $myArray = array();
 
-$res = $mysqli->query("SELECT term FROM slang");
+$res = $mysqli->query("SELECT term, def FROM slang");
 
 while ($row = $res->fetch_assoc()) {
 	array_push($myArray, $row);
 }
 
-for ($i = 0; $i<2; $i++ ) {
-	echo $myArray[$i]['word'];
-}
+echo $myArray[0]['term'];
+
+echo $myArray[0]['def'];
+
+// for ($i = 0; $i<2; $i++ ) {
+// 	echo $myArray[$i]['term'];
+// }
 ?>
 
 
 
 <!---------------------------------------------------------->
 
-?>
+
 <?php
     include("footer.php");
 ?>
