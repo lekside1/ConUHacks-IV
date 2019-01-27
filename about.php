a719794a4ddfb48099c5f0d50da6548d7f86d19b
Ernest Nguyen <erniiiiie@ernests-mbp.wireless.concordia.ca>
2019-01-26 21:25:10 -0500

    Added about page

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php
+    // header
+    $page_title = "About the Quebec Slang Translator";
+    include('header.php');
+?>
+
+<p>
+    The <em>Quebec Slang Translator</em> is a website which translates French Quebec 
+    slang into English so that the English-speaking minority in Quebec is able to better understand
+    the French that is often spoken in Quebec. Our purpose is to assure that speakers of all
+    languages should be able to communicate with each other in some way or another, and we
+    believe that this website is a step toward our goal.
+</p>
+<p>
+    This project has been designed and programmed in 24 hours at ConUHacks IV by 
+    Adelekan Faruq Aliu, Gia-Khang Ernest Nguyen, Kennan Hoa and Celia Cai as a submission
+    for the themed challenge from Canada Heritage, Governement of Canada. HTML, CSS,
+    JavaScript and PHP were the programming languages used to code this project.  
+    Also, the team used GitHub to manage version control of the project.   
+</p>
+
+<?php
+    // footer
+    include('footer.php');
+?>
+
